71eefe2d7dc2c50806fe7e287761369a1e4111ca
Nelyra <65348880+Nelyra@users.noreply.github.com>
2022-06-03 01:24:24 +0200

    nouvelles commandes ouais

--- shinegg/libs/userUtil.php
+++ shinegg/libs/userUtil.php
@@ -75,5 +75,116 @@ function getUserName($id)
     return SQLGetChamp($SQL);
 }
 
+function mostRecentMatch($id) {
+	
+	$SQL="SELECT player1_score, player2_score, player1_id, player2_id, tournament_id 
+	FROM matches
+	WHERE player1_id='$id' OR player2_id='$id'
+	ORDER BY match_id DESC";
+
+	$Request = parcoursRs(SQLSelect($SQL))[0];
+
+	if (!$Request) {
+		return false;
+	} 
+
+	return $Request;
+	
+}
+
+function getUserMatches1($id) {
+	
+	$SQL="SELECT player1_score, score_max
+	FROM matches
+	WHERE player1_id='$id'
+	ORDER BY match_id DESC";
+
+	$Request = parcoursRs(SQLSelect($SQL));
+
+	if (!$Request) {
+		return false;
+	} 
+
+	return $Request;
+}
+
+function getUserMatches2($id) {
+	
+	$SQL="SELECT player2_score, score_max
+	FROM matches
+	WHERE player2_id='$id'
+	ORDER BY match_id DESC";
+
+	$Request = parcoursRs(SQLSelect($SQL));
+
+	if (!$Request) {
+		return false;
+	} 
+
+	return $Request;
+}
+
+function changeNameUser($id, $pseudo) {
+
+	$SQL="UPDATE users
+	SET name = '$pseudo' 
+	WHERE id = '$id';";
+
+SQLUpdate($SQL);
+}
+
+function changePDPUser($id, $pdp) {
+
+	$SQL="UPDATE users
+	SET profile_picture = '$pdp' 
+	WHERE id = '$id';";
+
+SQLUpdate($SQL);
+}
+
+function changeTeamUser($id, $team) {
+
+	$SQL="UPDATE users
+	SET team = '$team' 
+	WHERE id = '$id';";
+
+SQLUpdate($SQL);
+}
+
+function changeMainUser($id, $main) {
+
+	$SQL="UPDATE users
+	SET main_char = '$main' 
+	WHERE id = '$id';";
+
+SQLUpdate($SQL);
+}
+
+function changePasswordUser($id, $password, $lastpassword) {
+
+	$SQL="SELECT password
+	FROM users
+	WHERE id = '$id';";
+
+	$Request = parcoursRs(SQLSelect($SQL));
+
+	if($Request == $lastpassword) {
+		$SQL="UPDATE users
+		SET password = '$password' 
+		WHERE id = '$id';";
+
+	SQLUpdate($SQL);
+	}
+
+}
+
+function changeBioUser($id, $bio) {
+
+	$SQL="UPDATE users
+	SET bio = '$bio' 
+	WHERE id = '$id';";
+
+SQLUpdate($SQL);
+}
 
 ?>
